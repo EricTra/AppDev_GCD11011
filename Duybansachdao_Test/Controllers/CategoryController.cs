a1baecb90d06a684b6a88a955fde8deff24669f4
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:27:46 +0700

    Create CategoryController.cs

---
+++ Duybansachdao_Test/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //private readonly ApplicationDBContext _dbContext
+        private readonly ICategoryRepository categoryRepository;
+        public CategoryController(ICategoryRepository category)
+        {
+            categoryRepository = category;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = categoryRepository.GetAll().ToList();
+            return View(categories);
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.Description)
+            {
+                ModelState.AddModelError("Name", "Name can not be equal to Description");
+            }
+
+            if (ModelState.IsValid)
+            {
+                categoryRepository.Add(category);
+                categoryRepository.Save();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public IActionResult Edit(int? idb)
+        {
+            if (idb == null || idb == 0)
+            {
+                return NotFound();
+            }
+            Category? category = categoryRepository.Get(c => c.CategoryId == idb);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepository.Update(category);
+                categoryRepository.Save();
+                TempData["success"] = "Category updated succesfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public IActionResult Delete(int? idb)
+        {
+            if (idb == null || idb == 0)
+            {
+                return NotFound();
+            }
+            Category? category = categoryRepository.Get(c => c.CategoryId == idb);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            categoryRepository.Remove(category);
+            categoryRepository.Save();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
