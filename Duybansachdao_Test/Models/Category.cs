e68852b944b9853ce0d3446ca2dbdf98a4f3c127
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:42:58 +0700

    Create Category.cs

---
+++ Duybansachdao_Test/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Duybansachdao_Test.Models
+{
+    public class Category
+    {
+        [Key] // nho set khoa chinh
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage ="Please enter category name")] //not null
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+       
+    }
+}
