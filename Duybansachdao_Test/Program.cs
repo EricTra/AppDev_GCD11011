5e91c3ea90a4b5a2a63a7cc07e0e8acb82c1b510
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:42:59 +0700

    Update Program.cs

--- Duybansachdao_Test/Program.cs
+++ Duybansachdao_Test/Program.cs
@@ -1,4 +1,7 @@
+using BookShopWeb.Repository;
 using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Repository;
+using Duybansachdao_Test.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(optionsAction=>
 optionsAction.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
