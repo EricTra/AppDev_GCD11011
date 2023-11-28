230a18553df3920db795fcbf7c7d5113e97c629e
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:27:54 +0700

    Create 20231121084258_seedingCategoryTable.cs

---
+++ Duybansachdao_Test/Migrations/20231121084258_seedingCategoryTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Duybansachdao_Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, "A man recuse the police", 1, "Action" },
+                    { 2, "A man recuse the police", 2, "Romance" },
+                    { 3, "A man recuse the police", 3, "Historical" },
+                    { 4, "A man recuse the police", 4, "Drama" },
+                    { 6, "A man recuse the police", 5, "Science" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 6);
+        }
+    }
+}
