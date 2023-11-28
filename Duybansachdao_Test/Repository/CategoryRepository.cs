b97819de655b0379a2a2094e79f17fcac7d3e617
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:43:03 +0700

    Create CategoryRepository.cs

---
+++ Duybansachdao_Test/Repository/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+
+namespace BookShopWeb.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryRepository(ApplicationDBContext dBContext) : base(dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public void Update(Category category)
+        {
+            _dbContext.Update(category);
+        }
+    }
+}
