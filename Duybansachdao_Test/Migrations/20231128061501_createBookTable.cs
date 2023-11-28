218da8ea4b9f3ef3a1cc9d8469170b089c9694d0
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:27:57 +0700

    Create 20231128061501_createBookTable.cs

---
+++ Duybansachdao_Test/Migrations/20231128061501_createBookTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Duybansachdao_Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class createBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                    table.ForeignKey(
+                        name: "FK_Books_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "Author", "CategoryId", "Description", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "F. Scott Fitzgerald", 6, "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald.", 10.99, "The Great Gatsby" },
+                    { 2, "John Steinbeck", 6, "The Grapes of Wrath is an American realist novel written by John Steinbeck and published in 1939.", 9.9900000000000002, "The Grapes of Wrath" },
+                    { 3, "George Orwell", 6, "Nineteen Eighty-Four: A Novel, often published as 1984, is a dystopian social science fiction novel by English novelist George Orwell.", 11.99, "Nineteen Eighty-Four" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
