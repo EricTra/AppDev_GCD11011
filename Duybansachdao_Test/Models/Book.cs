f63c51e8b873f01b5654ffff7099d283dc8105b3
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:42:54 +0700

    Create Book.cs

---
+++ Duybansachdao_Test/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Duybansachdao_Test.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public Book() { }
+    }
+}
