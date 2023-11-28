48c214866a93ca193658d75829fc9bd22f2bb562
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:43:01 +0700

    Create BookRepositorycs.cs

---
+++ Duybansachdao_Test/Repository/BookRepositorycs.cs
@@ -0,0 +1,25 @@
+﻿using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+using Duybansachdao_Test.Repository;
+using BookShopWeb.Repository;
+
+namespace Duybansachdao_Test.Repository
+{
+    public class BookRepository : Repository<Book>, IBookRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public BookRepository(ApplicationDBContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(Book book)
+        {
+            _dbContext.Update(book);
+        }
+    }
+}
