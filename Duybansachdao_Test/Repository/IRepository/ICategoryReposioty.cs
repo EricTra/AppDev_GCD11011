651230d27a4d04f5159df648e1d2860184847cbf
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:43:09 +0700

    Create ICategoryReposioty.cs

---
+++ Duybansachdao_Test/Repository/IRepository/ICategoryReposioty.cs
@@ -0,0 +1,11 @@
+﻿using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+
+namespace Duybansachdao_Test.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
