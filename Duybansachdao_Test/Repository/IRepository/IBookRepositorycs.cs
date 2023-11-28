bdcd2e087267c3a3df89832f4841f66772845c44
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:43:07 +0700

    Create IBookRepositorycs.cs

---
+++ Duybansachdao_Test/Repository/IRepository/IBookRepositorycs.cs
@@ -0,0 +1,11 @@
+﻿using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Models;
+using Duybansachdao_Test.Repository.IRepository;
+
+namespace Duybansachdao_Test.Repository.IRepository
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        void Update(Book book);
+    }
+}
