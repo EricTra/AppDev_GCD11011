d92c2b5d416efd67415171101b93ea22000d7295
EricTra <tranquangduy246@gmail.com>
2023-11-28 15:43:17 +0700

    Create UnitOfWorks.cs

---
+++ Duybansachdao_Test/Repository/UnitOfWorks.cs
@@ -0,0 +1,19 @@
+﻿using Duybansachdao_Test.Data;
+using Duybansachdao_Test.Repository.IRepository;
+namespace Duybansachdao_Test.Repository
+{
+    public class UnitOfWorks : IUnitOfWork
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public ICategoryRepository CategoryRepository => throw new NotImplementedException();
+
+        public IBookRepository Book => throw new NotImplementedException();
+
+        public ICategoryRepository Category => throw new NotImplementedException();
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
